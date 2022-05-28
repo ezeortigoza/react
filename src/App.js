c0ca47a326a1b04e70f2a6575dbe1bf7dff86779
ezeortigoza <eortigozaportillo@gmail.com>
2022-05-28 17:36:58 -0300

    new commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Bienvenidos a mi app
         </p>
